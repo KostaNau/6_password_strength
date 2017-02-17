ee63db8e242ccdad939ce0154138bfb7a4079f15
KostaNau <naumov.k@gmail.com>
2017-02-17 15:20:56 +0300

    Global changes. Merged get_local/remote_blacklist funcition in one generator, removed is_local_blacklist and is_password_in_blacklist, rewited business logic, fix some typo

--- password_strength.py
+++ password_strength.py
@@ -1,8 +1,11 @@
 import argparse
 from getpass import getpass
-import requests
+from itertools import chain
 import socket
 import re
+
+import requests
+
 
 
 REMOTE_BLACKLISTS = {
@@ -19,31 +22,20 @@ def is_connected():
     port = 80
     timeout = 2
     try:
-        print('Checking your connection to the Internet...')
         s = socket.create_connection((test_host, port), timeout)
-        print('Connected.\n')
         s.close()
-    except Exception:
-        print("No connection. Attention!!! Script will work without verification remote blacklists.\n" )
+    except socket.gaierror:
         return False
     return True
 
 
-def is_local_blacklist(filepath):
-    if filepath:
-        return True
-    else:
-        print('Local blacklist has not been declared by user. Script will works without verification local blacklist.\n')
-        return False
-
-
 def collect_presonal_data():
     personal_data = {
-        'Given Name': None,
-        'Family Name': None,
-        'Date of Brith':None,
-        'Your Phone Number': None,
-        'City of Brith': None
+        '1. Given Name': None,
+        '2. Family Name': None,
+        '3. City of Birth': None,
+        '4. Date of Brith':None,
+        '5. Your Phone Number': None,
         }
 
     for question in sorted(personal_data):
@@ -52,30 +44,19 @@ def collect_presonal_data():
     return personal_data
 
 
-def get_remote_blacklist(remote_links):
-    for list_link in remote_links.values():
-        remote_blacklist = requests.get(list_link).text.rsplit()
-        yield remote_blacklist
-
-
-def get_local_blacklist(local_path):
-    with open(local_path, 'r') as blacklist_file:
-        local_blacklist = blacklist_file.read().rsplit()
-        return local_blacklist
-
-
-def is_password_in_blacklists(password, *blacklists_stack):
-    for black_lists in blacklists_stack:
-        if black_lists == None:
-            continue
-        for black_list in black_lists:
-            if password in black_list:
-                return False
-    return True
+def get_blacklists(remote_links, local_path=False):
+    if local_path:
+        with open(local_path, 'r') as blacklist_file:
+            local_blacklist = blacklist_file.read().rsplit()
+            yield from local_blacklist
+    if is_connected():
+        for list_link in remote_links.values():
+            remote_blacklist = requests.get(list_link).text.rsplit()
+            yield from remote_blacklist
 
 
 def get_password_strength(password, personal_data, blacklist_inspection):
-    strength_password = 0
+    strength_password = {}
     strength_criteria = {
                     '"digit"': '(?=.*?\d)',
                     '"uppercase"': '(?=.*?[A-Z])',
@@ -84,32 +65,33 @@ def get_password_strength(password, personal_data, blacklist_inspection):
                     '"special characters"': '\W+'
                     }
 
-    if blacklist_inspection and password not in personal_data.values():
-        print('Passed test of: "into blacklists and personal data"')
+    if not blacklist_inspection and password not in personal_data.values():
         for name, pattern in strength_criteria.items():
             response_status = re.search(pattern, password)
             if response_status:
-                print('Passed test of: {}'.format(name))
-                strength_password += 2
-            else:
-                print('Not passed test of: {}'.format(name))
+                strength_password[name] = 2
         return strength_password
     else:
-        print('Your password has been found in some of blacklists or your personal data:')
         return 0
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Password Strength Calculator')
-    parser.add_argument('-l', '--local', help="path to local blacklist or filename for current directory.")
+    parser = argparse.ArgumentParser(
+            description='Password Strength Calculator')
+    parser.add_argument('-l', '--local',
+                        default=None,
+                        help="path to local blacklist or filename for current directory.")
     args = parser.parse_args()
 
-    local_blacklist = get_local_blacklist(args.local) if is_local_blacklist(args.local) else None
-    remote_blacklist = get_remote_blacklist(REMOTE_BLACKLISTS) if is_connected() else None
-
-    personal_data = collect_presonal_data()
     usr_password = getpass(prompt='Type your password: ')
-    print('Calculating strength of your password, please wait...')
-
-    password_rating = get_password_strength(usr_password, personal_data, is_password_in_blacklists(usr_password, local_blacklist, remote_blacklist))
-    print('Strength of your password: {} of 10\n'.format(password_rating))
+    personal_data = collect_presonal_data()
+    blacklists = get_blacklists(REMOTE_BLACKLISTS, args.local)
+    print('Please wait...')
+    blacklist_inspection_result = any(usr_password in x for x in blacklists)
+    password_rating = get_password_strength(usr_password, personal_data, blacklist_inspection_result)
+    if password_rating == 0:
+        print('Your password has been found in some of blacklists or your personal data. Please try again!')
+    else:
+        print('Strength of your password: {} of 10'.format(sum(password_rating.values())))
+        for test in password_rating.keys():
+            print('Passed test of {}'.format(test))
